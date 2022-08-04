2b5079f71648df3c0c6920a7e5735e21904728fa
8BitStick <little.bit.sticky@gmail.com>
2022-08-05 01:25:31 +1000

    fixed nextTide indexes

--- screens/Tides.js
+++ screens/Tides.js
@@ -9,9 +9,8 @@ import { getTides } from '../redux/actions';
 
 
 const Tides = ({ route }) => {
-    const [nextTide, setNextTide] = useState()
-    const [timeUntilNextTide, setTimeUntilNextTide] = useState()
-    const [loading, setLoading] = useState(true)
+    const [nextTide, setNextTide] = useState(null)
+    const [timeUntilNextTide, setTimeUntilNextTide] = useState(null)
     const { tideDays } = useSelector(state => state.tidesReducer);
     const dispatch = useDispatch();
     const fetchTides = () => dispatch(getTides(item.station_id, item.region))
@@ -22,13 +21,13 @@ const Tides = ({ route }) => {
     
     useEffect(() => {
         if(isFocused && item){
-            fetchTides()
-            calcNextTide()
-            setLoading(false)
+            fetchTides();
+            calcNextTide();
         }
     }, [isFocused, item])
 
-    const calcNextTide = async () => {
+
+    const calcNextTide = async() => {
         if (tideDays !== undefined) {
             const arr = []
             tideDays.map((tideDay) => {
@@ -50,22 +49,21 @@ const Tides = ({ route }) => {
                 return a.tideTime - b.tideTime
             })
             const tideDaysIndex = closest[0].tideDayId
-            const tidesIndex = closest[0].tideId
-            const minutes = closest[0].timeDiff % 60
-            const hours = Math.floor(closest[0].timeDiff / 60)
+            const tidesIndex = closest[0].tideId - 1
+            const minutes = ( closest[0].timeDiff % 60 ) + 1
+            const hours = Math.floor((closest[0].timeDiff + 1) / 60)
             setTimeUntilNextTide({
                 hours: hours,
                 minutes: minutes
             })
             setNextTide(tideDays[tideDaysIndex].tides[tidesIndex])
-            
+
         }
     }
 
-    
+  
 
-    if (!loading) {
-        
+    if(nextTide){
         return (
             <SafeAreaView>
                 <ScrollView>
@@ -77,32 +75,28 @@ const Tides = ({ route }) => {
                             </Box>
                             <Box flex={1} flexDirection="row-reverse" >
                                 <Text fontSize="lg" fontWeight={700} color="#16688d">
-                                    Next Tide: <Text fontSize="xl" color={nextTide.tide_type === "High" ? "#f05c2c" : "#16688d"}>
+                                    Next Tide: <Text fontSize="xl" color={nextTide.tide_type === "High" ? "#16688d" : "#f05c2c"}>
                                         {nextTide.tide_type}
                                     </Text>
                                 </Text>
                             </Box>
                             <Box flex={1} flexDirection="row-reverse">
                                 <Text fontSize="lg" fontWeight={700} color="#16688d">
-                                    {timeUntilNextTide.hours} hrs {timeUntilNextTide.minutes} mins
+                                    {timeUntilNextTide.hours === "0" ? "" : `${timeUntilNextTide.hours} hrs`} {timeUntilNextTide.minutes === "60" ? "" : `${timeUntilNextTide.minutes} mins`}
                                 </Text>
                             </Box>
                             <Box margin={2}>
                                 {tideDays.map((tideDay, i) => (
-                                    <Tide key={tideDay.id} tideDay={tideDay} />
+                                    <Tide key={tideDay.id} tideDay={tideDay}  />
                                 ))}
                             </Box>
-                        </Box>     
+                        </Box>
                     </NativeBaseProvider>
                 </ScrollView>
-            </SafeAreaView>    
-        )
-    } else {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size="large" />
-            </View >
+            </SafeAreaView>
         )
     }
+        
+    
 }
 export default Tides
